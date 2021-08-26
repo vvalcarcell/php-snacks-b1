5170929e958c0d463d7c8ba6d8fc4c770e4917e3
vvalcarcell <valcarcellvalentina@gmail.com>
2021-08-26 17:13:38 +0200

    snack 5

--- 5.php
+++ 5.php
@@ -1,11 +1,14 @@
 <?php
 
+    $text= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ea, facere esse laborum impedit dolore deleniti modi, aliquid quos a nihil repudiandae perspiciatis laudantium. Culpa minus esse commodi necessitatibus eius.";
+    
+    $textArray = explode(".", $text);
+
+    for($i=0; $i < count($textArray)-1; $i++) {
+
+        echo "<p> {$textArray[$i]}. </p>";
+
+    }
 
     
-     
-
-
-
-
-
- ?>
+?>
